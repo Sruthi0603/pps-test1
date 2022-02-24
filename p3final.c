55d8ce152347e4bd799988b4b1d8b5322600d9a7
SruthiD <97238920+Sruthi0603@users.noreply.github.com>
2022-02-24 11:24:01 +0000

    did a code to add 2 numbers by call by reference

--- p3final.c
+++ p3final.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-void input(int x,int y);
+void input(int *x,int *y)
 {
   printf("enter two numbers:");
-  scanf("%d %d",x,y);
+  scanf("%d%d",x,y);
 }
-void sum(int x,int y,int z);
+void sum(int x,int y,int *z)
 {
-  z=x+y;
+  *z=x+y;
 }
-void output(int x,int y,int z);
+void output(int x,int y,int z)
 {
   printf("%d+%d=%d",x,y,z);
 }
-int main(int a,int b,int c);
+int main()
 {
+  int a,b,c;
   input(&a,&b);
   sum(a,b,&c);
   output(a,b,c);
